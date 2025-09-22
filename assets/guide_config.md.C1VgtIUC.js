638ef41a0790b51a5dd1afd901c14222a19fb222
zhouxule <bread.zhou@hotmail.com>
2025-09-22 16:38:59 +0800

    deploy

---
+++ assets/guide_config.md.C1VgtIUC.js
@@ -0,0 +1,14 @@
+import{_ as a,c as n,o as i,ae as e}from"./chunks/framework.DWhDcyZJ.js";const k=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/config.md","filePath":"guide/config.md"}'),p={name:"guide/config.md"};function l(t,s,c,d,h,o){return i(),n("div",null,[...s[0]||(s[0]=[e(`<p>### <code>guide/config.md</code></p><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">\\#</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 配置</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">在 </span><span style="--shiki-light:#005CC5;--shiki-dark:#79B8FF;">\`.env\`</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 文件中修改配置：</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">\`\`\`ini</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">VITE\\_API\\_URL=https://api.example.com</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">VITE\\_TOKEN=xxxxxx</span></span></code></pre></div>`,2)])])}const E=a(p,[["render",l]]);export{k as __pageData,E as default};
---
+++ assets/guide_deploy.md.D3ndmn-i.js
@@ -0,0 +1,16 @@
+import{_ as i,c as a,o as n,ae as e}from"./chunks/framework.DWhDcyZJ.js";const c=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/deploy.md","filePath":"guide/deploy.md"}'),l={name:"guide/deploy.md"};function t(p,s,h,d,k,o){return n(),a("div",null,[...s[0]||(s[0]=[e(`<h3 id="guide-deploy-md" tabindex="-1"><code>guide/deploy.md</code> <a class="header-anchor" href="#guide-deploy-md" aria-label="Permalink to &quot;\`guide/deploy.md\`&quot;">​</a></h3><div class="language-md vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">md</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;"># 部署</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">推荐使用 </span><span style="--shiki-light:#24292E;--shiki-light-font-weight:bold;--shiki-dark:#E1E4E8;--shiki-dark-font-weight:bold;">**Vercel**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 或 </span><span style="--shiki-light:#24292E;--shiki-light-font-weight:bold;--shiki-dark:#E1E4E8;--shiki-dark-font-weight:bold;">**Netlify**</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 部署。</span></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#005CC5;--shiki-light-font-weight:bold;--shiki-dark:#79B8FF;--shiki-dark-font-weight:bold;">## Vercel</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">1.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 登录 Vercel</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">2.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 导入 GitHub 仓库</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#E36209;--shiki-dark:#FFAB70;">3.</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"> 一键部署</span></span></code></pre></div>`,2)])])}const g=i(l,[["render",t]]);export{c as __pageData,g as default};
